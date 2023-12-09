b3c7293ee6ae759e6a637944e03e7b42e88e868d
okHadi <khan.hadi2951@gmail.com>
2023-12-09 14:18:55 +0500

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 4306;
 
+
+app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }))
 
