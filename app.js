7b4f4a93cea9f518fe95620256852e8a9b7f50f2
okHadi <khan.hadi2951@gmail.com>
2023-12-23 17:59:41 +0500

    config updated

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 const port = 4306;
 
 
+var corsOptions = {
+    origin: 'https://nustutor.mooo.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }))
