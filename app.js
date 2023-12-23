bb9242fa609b14641d4da7cbb41d6ffa6e681aa6
okHadi <khan.hadi2951@gmail.com>
2023-12-23 18:00:55 +0500

    CORS added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }))
 
