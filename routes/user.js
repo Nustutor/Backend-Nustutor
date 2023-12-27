4e328a48c825494fa560c2c052e26455aa4b0c3f
okHadi <khan.hadi2951@gmail.com>
2023-12-28 00:21:59 +0500

    user api fix

--- routes/user.js
+++ routes/user.js
@@ -19,17 +19,20 @@ router.get('/', auth, async (req, res) => {
         emailVerificationCode,
         bio
     FROM
-        users;`
+        users
+    WHERE
+        uuid = UUID_TO_BIN(?)
+        ;`
         db.query(getUserQuery, [uuid], (err, results) => {
             if (err) {
-                console.error('Error getting tutor:' + err);
-                return res.status(500).json({ error: 'Internal Server Error when getting tutor' + err });
+                console.error('Error getting user:' + err);
+                return res.status(500).json({ error: 'Internal Server Error when getting user' + err });
             }
             res.status(200).json({ results })
         })
     }
     catch (error) {
-        res.status(500).json({ error: 'Internal Server Error when getting tutor' + error })
+        res.status(500).json({ error: 'Internal Server Error when getting user' + error })
     }
 });
 
