5286e50afbf1bff3f3bdc6e3bd078655cfb85e1c
Khanh <khan.hadi2951@gmail.com>
2024-05-07 01:45:20 +0500

    fixed signup email

--- routes/signup.js
+++ routes/signup.js
@@ -70,14 +70,13 @@ router.post('/', async (req, res) => {
                     const verificationLink = "https://nustutor.mooo.com/verifyEmail/" + emailVerificationCode + "/";
                     const emailHTML = getVerificationEmailString(fullname, verificationLink)
                     const transporter = nodemailer.createTransport({
-                        service: 'Gmail',
+                        service: 'gmail',
+                        host: 'smtp.gmail.com',
+                        port: 465,
+                        secure: true,
                         auth: {
-                            type: 'OAuth2',
                             user: process.env.EMAIL,
-                            clientId: process.env.GOOGLE_CLIENT_ID,
-                            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                            refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-
+                            pass: process.env.APP_PASSWORD,
                         },
                     });
 
